a7c28c894820d9b2782038329649b85acaafa168
aljo242 <48221223+aljo242@users.noreply.github.com>
2019-10-25 11:25:29 -0400

    Looking into float vs quant
    Issues arisen with using full quant models

---
+++ tflite_googlenet_conv_float.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Layer
+import os
+from PIL import Image
+import numpy as np
+import imageio
+
+
+def representative_dataset_gen():
+  for _ in range(100):
+      img = imageio.imread('cat.jpg', pilmode='RGB')
+      img = np.array(Image.fromarray(img).resize((120, 120))).astype(np.float32)
+      img[:, :, 0] -= 123.68
+      img[:, :, 1] -= 116.779
+      img[:, :, 2] -= 103.939
+      img[:, :, [0, 1, 2]] = img[:, :, [2, 1, 0]]
+      data = np.expand_dims(img, axis=0)
+      yield [data]
+
+model = tf.keras.models.load_model("hccr_googlenet_keras.h5")
+model.summary()
+
+# converting currently with OPTIMIZE FOR SIZE argument
+# With snippets of sample data, will be able to call a full model (weights and other ops) quantization
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset_gen
+print("Converting and Quantizing...")
+tflite_model = converter.convert()
+
+print("Saving tflite file...")
+open("hccr_googlenet_lite_float.tflite", "wb").write(tflite_model)
